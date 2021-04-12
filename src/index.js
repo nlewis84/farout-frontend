6fb564c1d4c928fc99ba4872c4b69f1248eda99b
nllew <n.lewis.perc@gmail.com>
2021-04-11 19:09:23 -0500

    added packages and setup index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(managePicture)
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
