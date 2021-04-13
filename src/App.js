96461abdb422df0907c6ad52966beb27615263a6
nllew <n.lewis.perc@gmail.com>
2021-04-12 19:55:48 -0500

    deleted erroneous files and setup PictureCardContainer.map

--- src/App.js
+++ src/App.js
@@ -6,24 +6,32 @@ import { fetchPictures } from './actions/fetchPictures'
 
 class App extends React.Component {
 
-  debugger
-
   componentDidMount() {
     this.props.fetchPictures()
   }
 
   render() {
-    return (
-      <div className="App" >
-        App
-        {/* write a map over this.props.pictures to create the two PictureCardContainers */}
-        {/* <PictureCardContainer picture={this.props.pictures[0]} /> */}
-        {/* <PictureCardContainer picture={this.props.pictures[1]} /> */}
-      </div>
-    );
+    if (!this.props.pictures.length) {
+      return (
+        <div className="App" >
+          <h1>FAR OUT</h1>
+          <h2>Loading...</h2>
+        </div>
+      )
+    } else {
+      return (
+        <div className="App" >
+          <h1>FAR OUT</h1>
+          <div>
+            {this.props.pictures.map(picture =>
+              <PictureCardContainer picture={picture} />
+            )}
+          </div>
+        </div>
+      );
+    }
   }
 }
-
 
 const mapStateToProps = state => {
   return {
