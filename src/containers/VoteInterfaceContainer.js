9d7e50024535183b7962d1773efb8bd5fea22e53
Nathan Lewis <n.lewis.perc@gmail.com>
2021-05-05 12:49:36 +0000

    reverted to worknig vote buttons

--- src/containers/VoteInterfaceContainer.js
+++ src/containers/VoteInterfaceContainer.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
 import VoteButton from '../components/VoteButton';
 import VoteCounter from '../components/VoteCounter';
-import { connect } from 'react-redux';
-import { increaseVoteCount } from '../actions/increaseVoteCount';
+// import { connect } from 'react-redux';
+// import { increaseVoteCount } from '../actions/increaseVoteCount';
 
 class VoteInterfaceContainer extends Component {
-    
-    handleClick = () => {
-        // debugger
-        this.setState({
-            ...this.state,
-            count: this.props.vote.count + 1
-        }, () => {
-            this.props.increaseVoteCount(this.state);
-            // debugger
-        })
-    }
+    // constructor(props) {
+    //     super(props);
+
+    //     this.state = {
+    //         count: '',
+    //         pictureId: props.vote.picture_id
+    //     };
+    //     // debugger
+    // }
+
+    // handleClick = () => {
+    //     // debugger
+    //     this.setState({
+    //         ...this.state,
+    //         count: this.props.vote.count + 1
+    //     }, () => {
+    //         this.props.increaseVoteCount(this.state);
+    //         // debugger
+    //     })
+    // }
 
     render() {
         return (
             <div className="vote-interface">
-                <VoteButton vote={this.props.vote} handleClick={this.props.handleClick} />
+                <VoteButton vote={this.props.vote} />
+                {/* <VoteButton vote={this.props.vote} handleClick={this.props.handleClick()} /> */}
                 <VoteCounter vote={this.props.vote} />
             </div>
         )
     }
 }
 
-export default connect(null, { increaseVoteCount })(VoteInterfaceContainer);
-// export default VoteInterfaceContainer;
+// export default connect(null, { increaseVoteCount })(VoteInterfaceContainer);
+export default VoteInterfaceContainer;
