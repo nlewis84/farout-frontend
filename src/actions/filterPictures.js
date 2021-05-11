dbee39c21481112d8ffb6ccd456c5d86849bd410
nllew <n.lewis.perc@gmail.com>
2021-05-11 00:24:13 -0500

    reverted to localhost addresses

--- src/actions/filterPictures.js
+++ src/actions/filterPictures.js
@@ -1,7 +1,7 @@
 export function filterPictures() {
     return (dispatch) => {
-        // fetch('http://localhost:3001/api/v1/pictures/filter')
-        fetch('https://farout-api.herokuapp.com/api/v1/pictures/top')
+        fetch('http://localhost:3001/api/v1/pictures/filter')
+        // fetch('https://farout-api.herokuapp.com/api/v1/pictures/top')
             .then(response => response.json())
             .then(pictures => dispatch({ type: 'FILTER_PICTURES', payload: pictures }));
     };
